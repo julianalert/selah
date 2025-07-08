d906ab63a498b44a626a51251da4a53d57879fc8
JulienD <juliend@JulienDs-MacBook-Pro.local>
2025-07-08 16:25:49 +0100

    fix tyuue

--- app/genre/[slug]/metadata.ts
+++ app/genre/[slug]/metadata.ts
@@ -2,7 +2,7 @@ export function generateMetadata({ params }: { params: { slug: string } }) {
     const genre = decodeURIComponent(params.slug);
     return {
       title: `${genre} AI Movies`,
-      description: `Watch the best AI-generated ${genre} short films.`,
+      description: `Watch the best AI-generated ${genre} short movies.`,
     };
   }
   
