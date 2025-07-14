454124dad4de31335c696ddd7e45f78491f22625
JulienD <juliend@JulienDs-MacBook-Pro.local>
2025-07-14 15:31:39 +0100

    big update adding series + admin dashboard

--- app/admin/series/[slug]/page.tsx
+++ app/admin/series/[slug]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import { supabase } from '../../../../lib/supabaseClient';
